aeb2c186428151d61e11cc233ee8ad6301c0a4f6
rick lee <1016821270@qq.com>
2019-07-31 16:38:12 +0800

    extract github config from code to properties

---
+++ src/main/java/blog/rick/community/community/controller/AuthorizeController.java
@@ -0,0 +1,43 @@
+package blog.rick.community.community.controller;
+
+import blog.rick.community.community.dto.AccessTokenDTO;
+import blog.rick.community.community.dto.GithubUser;
+import blog.rick.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Description: community
+ * Created by lizhenguo on 2019/7/31 10:42
+ */
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                            @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
---
+++ src/main/java/blog/rick/community/community/dto/GithubUser.java
@@ -0,0 +1,35 @@
+package blog.rick.community.community.dto;
+
+/**
+ * Description: community
+ * Created by lizhenguo on 2019/7/31 15:31
+ */
+public class GithubUser {
+    private String name;
+    private long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
